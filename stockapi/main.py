14af20be8f5a8744042fa65ade5ee090bdaf2114
mayurigupta01 <mayuri.gupta01>
2022-05-08 20:17:03 -0700

    code

--- stockapi/main.py
+++ stockapi/main.py
@@ -1,4 +1,4 @@
 from stockapi import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context=('./cert.pem', './key.pem'))
+    app.run(host='0.0.0.0', port=443, ssl_context=('/home/ec2-user/code/wall_street_trade/stockapi/cert.pem', '/home/ec2-user/code/wall_street_trade/stockapi/key.pem'))
