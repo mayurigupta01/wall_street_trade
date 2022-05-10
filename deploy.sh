1fdb590216381050977a011b2c2394c665b846db
mayurigupta01 <mayuri.gupta01>
2022-05-10 10:12:02 -0700

    deploy.sh for web servers

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 ASG_NAME="WallStreet-WebServerAsg"
-REGION="us-west-2"
 echo "Identifying ASG $ASG_NAME current capacity"
-desired_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $REGION --query 'AutoScalingGroups[].[DesiredCapacity]' --output text`
-min_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $REGION --query 'AutoScalingGroups[].[MinSize]' --output text`
-max_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $REGION --query 'AutoScalingGroups[].[MaxSize]' --output text`
+region="us-west-2"
+desired_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $region --query 'AutoScalingGroups[].[DesiredCapacity]' --output text`
+min_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $region --query 'AutoScalingGroups[].[MinSize]' --output text`
+max_size=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --region $region --query 'AutoScalingGroups[].[MaxSize]' --output text`
 
 echo "Min Size: $min_size"
 echo "Max Size: $max_size"
 echo "Desired Size: $desired_size"
 
 # Set ASG desired capacity above to 0
-echo "Setting the values for ASG capacity to 0, and starting app upgrade with latest app image for ASG: $ASG_NAME"
+echo "Setting the values for ASG capacity to 0, and starting app upgrade with latest app image $ASG_NAME"
 aws autoscaling update-auto-scaling-group \
-    --region $REGION \
-    --auto-scaling-group-name $ASG_NAME \
+    --region us-west-2 \
+    --auto-scaling-group-name WallStreet-WebServerAsg \
     --min-size 0 \
     --max-size 0 \
     --desired-capacity 0
@@ -24,12 +24,10 @@ aws autoscaling update-auto-scaling-group \
 sleep 300
 echo "Setting the value to desired capacity again to MinSize $min_size MaxSize $max_size DesiredSize $desired_size"
 aws autoscaling update-auto-scaling-group \
-    --region $REGION \
-    --auto-scaling-group-name $ASG_NAME \
+    --region us-west-2 \
+    --auto-scaling-group-name WallStreet-WebServerAsg \
     --min-size $min_size \
     --max-size  $max_size\
     --desired-capacity $desired_size
-
-
 
 
